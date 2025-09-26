3c6ee28d8054494b14226b2075739562ea199c97
mahdi <ma13842791199@gmail.com>
2025-09-26 17:49:05 +0330

    added project

---
+++ script.js
@@ -0,0 +1,31 @@
+let countrySelectBox = document.querySelector('.countrySelect')
+let citySelect = document.querySelector('.citySelect')
+
+let countriesData = {
+    Iran: ['tabriz', 'tehran', 'khoy', 'esfahan', 'mashhad'],
+    Turkey:['Istanbul', 'ezmir', 'ankara', 'antaliya'],
+    US:['vashangton', 'niyoyork', 'tegzas', 'losangles']
+}
+
+countrySelectBox.addEventListener('change', function(){
+
+    if(countrySelectBox.value === "Please Select"){
+       citySelect.innerHTML = '';
+       citySelect.innerHTML += "<Option>select city</Option>";
+
+    } else {
+        let mainCountryName = countrySelectBox.value
+        let mainCountrycities = (countriesData[mainCountryName]);
+
+        citySelect.innerHTML = '';
+
+        mainCountrycities.forEach(function (city) {
+            // console.log(city)
+
+            citySelect.innerHTML += '<option>' + city + '</option>'
+        });
+    }
+
+    
+});
+
